9e32510dc925d30b83b4e1beb74a11fe543caa0f
Lorenzo <lorenzo.cerreta@libero.it>
2023-01-11 19:57:37 +0100

    fix typo

--- exercises/PyExercises/NameSurnameEx16.py
+++ exercises/PyExercises/NameSurnameEx16.py
@@ -1,4 +1,4 @@
-##With a given integral number n, write a program to generate a dictionary
+##With a given integer number n, write a program to generate a dictionary
 ##that contains (i, i*i) such that is an integral number between 1 and
 ##n (both included). and then the program should print the dictionary.
 
